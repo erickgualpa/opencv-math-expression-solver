c56b6d4fc77a097e2678cf36b502a5dd4d83b64a
Erick Gualpa <m.erickgualpa@gmail.com>
2021-02-05 00:31:15 +0100

    Distribute files in directories

--- digit-symbols-cnn-classif-training-framework/digits_symbols_classifier_cnn.py
+++ digit-symbols-cnn-classif-training-framework/digits_symbols_classifier_cnn.py
@@ -6,7 +6,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
-from DigitsSymbolsDataset import DigitsSymbolsDataset
+from dataset.DigitsSymbolsDataset import DigitsSymbolsDataset
 
 K.clear_session()
 
